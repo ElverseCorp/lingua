4b490700450c0d9ac733af3f84b8ec62a003d392
robotsvision <robotsvision.vx@gmail.com>
2024-02-27 22:32:37 +0600

    lingua -> basics and llibs support

--- include/lingua.h
+++ include/lingua.h
@@ -9,9 +9,18 @@
  * @author   Matvey Rybalkin
 ******************************************************************************/
 
+/* Check C standard */
+#if __STDC_VERSION__  >= 201112L
+
 /* Check Llibs framework */
 #ifndef LLIBS_FRAMEWORK_H_
 #include "foundation/include/foundation.h"
 #endif
 
+/* Include standard Lingua libraries */
+#include "utf8.h"
+
+#else /* ERROR (__STDC_VERSION__ >= 201112L) */
+#error [lingua.h]: Requires at least version C 2011 (__STDC_VERSION__ >= 201112L)
+#endif
 #endif // LINGUA_FRAMERWORK_H_
---
+++ include/utf8.h
@@ -0,0 +1,14 @@
+#ifndef LINGUA_FRAMERWORK_UTF_8_H_
+#define LINGUA_FRAMERWORK_UTF_8_H_
+
+/**************************************************************************//**
+ * @file     utf8.h
+ * @brief    Project L - Lingua framework, main UTF-8 encode support header.
+ * @version  V0.0.1
+ * @date     26. Feb 2024
+ * @author   Matvey Rybalkin
+******************************************************************************/
+
+/* Must be included in lingua.h */
+
+#endif // LINGUA_FRAMERWORK_UTF_8_H_
